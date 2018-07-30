37476f19efdd79a50b79125d0ddc04d0aee7f315
martmists <martmists@gmail.com>
2018-07-30 19:21:18 +0200

    Remove case sensitivity

--- src/main/kotlin/com/martmists/packend/extensions/List.kt
+++ src/main/kotlin/com/martmists/packend/extensions/List.kt
@@ -3,10 +3,10 @@ package com.martmists.packend.extensions
 fun List<String>.containsFile(filepath: String): Boolean{
     for (file in this) {
         if (file.endsWith("/")) {
-            if (filepath.contains(file))
+            if (filepath.toLowerCase().contains(file))
                 return true
         } else {
-            if (filepath.endsWith(file))
+            if (filepath.toLowerCase().endsWith(file))
                 return true
         }
     }
